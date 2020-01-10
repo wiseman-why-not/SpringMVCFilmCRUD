df1a66136d5c6dae72118d6a544d915338822902
wiseman-why-not <marcus.e.wiseman@gmail.com>
2020-01-10 16:25:07 -0700

    First commit

---
+++ src/test/java/com/skilldistillery/film/entities/FilmTests.java
@@ -0,0 +1,14 @@
+package com.skilldistillery.film.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FilmTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
